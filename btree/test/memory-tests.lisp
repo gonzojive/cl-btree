c50a519d06838d6b5e9ba218f9cc2d534f305580
Red Daly <reddaly@gmail.com>
2009-11-12 03:34:20 -0800

    New test cases, too.

--- btree/test/memory-tests.lisp
+++ btree/test/memory-tests.lisp
@@ -56,3 +56,57 @@
     (dotimes (i n)
       (btree:btree-insert b i (format nil "Value ~A" i)))
     b))
+
+(deftest ordered-in-ordered-out (ordered-list &key (max-keys 3))
+  (let ((b (make-instance 'btree::memory-btree :max-keys max-keys))
+	out)
+    (dolist (i ordered-list)
+      (btree:btree-insert b i i))
+    (btree:btree-map b #'(lambda (k v) (push k out)))
+    (is (equalp ordered-list (nreverse out)))))
+
+(deftest sorts-like-sort (list &key (max-keys 3) (predicate #'<))
+  (let ((b (make-instance 'btree::memory-btree :max-keys max-keys :predicate predicate))
+	out)
+    (dolist (i list)
+      (btree:btree-insert b i i))
+    (btree:btree-map b #'(lambda (k v) (push k out)))
+    (is (equalp (sort (copy-list list) predicate)
+		(nreverse out)))))
+
+(defun ordered-listn (n)
+  (loop :for i :from 0 :upto n :collect i))
+
+(deftest many-ordered-when-mapped ()
+  (ordered-in-ordered-out (ordered-listn 3))
+  (ordered-in-ordered-out (ordered-listn 12))
+  (ordered-in-ordered-out (ordered-listn 30))
+  (ordered-in-ordered-out (ordered-listn 3000) :max-keys 15)
+  (ordered-in-ordered-out (ordered-listn 3000) :max-keys 15)))
+
+(defun plist->btree (plist &rest btree-args)
+  (let ((b (apply #'make-instance 'btree::memory-btree btree-args)))
+    (dolist (keyval (alexandria:plist-alist plist))
+      (btree:btree-insert b (car keyval) (cdr keyval)))
+    b))
+
+(deftest mapping-values-hits ()
+  (let ((b (plist->btree '(1 1 2 4))))
+    (is 1 (btree-search b 1))
+    (is 4 (btree-search b 2))))
+
+(deftest mapping-values-hits2 ()
+  (let ((b (plist->btree (loop :for i :from  0 :upto 300 :collect i :collect (+ i 10)))))
+    (is 110 (btree-search b 100))
+    (is 12 (btree-search b 2))))
+
+(deftest mapping-values-misses ()
+  (let ((b (plist->btree (loop :for i :from  0 :upto 300 :collect i :collect (+ i 10)))))
+    (with-expected-failures
+      (is (null (btree-search b -100)))
+      (is (null (btree-search b 100.3))))))
+
+(deftest sorting ()
+  (sorts-like-sort '(3 4 13 6 2 3 14 6))
+  (sorts-like-sort (mapcar #'(lambda (x) (format nil "blah blah ~A blah" x)) '(3 4 13 6 2 3 14 6))
+		   :predicate #'string-lessp))
